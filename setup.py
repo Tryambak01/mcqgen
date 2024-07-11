8ad250cf2ed2772168807ca9e85ec1f85cf9f593
Tryambak01 <tryambakaadiga@gmail.com>
2024-07-11 09:29:36 +0530

    Updated backend

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 # this file installs local packages in the virtual environment 
+
+from setuptools import find_packages, setup
+
+setup (
+    name= "mcqgenerator",
+    version= "0.0.1",
+    author= "Tryambak",
+    author_email= "tryambakaadiga@gmail.com",
+    install_requires = ["openai", "langchain", "streamlit", "python-dotenv", "pyPDF2"],
+    packages= find_packages()
+)
